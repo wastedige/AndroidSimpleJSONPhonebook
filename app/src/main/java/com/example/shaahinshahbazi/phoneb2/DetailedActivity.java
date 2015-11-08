98f29b687c030403fa5ce84163cfd6f66ec27625
Shaahin Shahbazi <shaahin@gmail.com>
2015-11-08 10:07:11 -0600

    Detailed page done. Returns to main on click

--- app/src/main/java/com/example/shaahinshahbazi/phoneb2/DetailedActivity.java
+++ app/src/main/java/com/example/shaahinshahbazi/phoneb2/DetailedActivity.java
@@ -4,6 +4,7 @@ package com.example.shaahinshahbazi.phoneb2;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,10 +23,11 @@ public class DetailedActivity extends Activity{
 
         TextView name = (TextView) findViewById(R.id.name);
         TextView company = (TextView) findViewById(R.id.company);
+        TextView email = (TextView) findViewById(R.id.email);
+        TextView website = (TextView) findViewById(R.id.website);
 
         Intent i = getIntent();
         // getting attached intent data
-
         jsonDetailedAddress = i.getStringExtra("json");
         name.setText(i.getStringExtra("name"));
         company.setText(i.getStringExtra("company"));
@@ -33,8 +35,21 @@ public class DetailedActivity extends Activity{
         String readJSON = jsonHandler.getJSON(jsonDetailedAddress);
         String [] detailedContent = jsonHandler.parseJsonDetailed(readJSON);
 
+        // using parsed JSON data
         new DownloadImageTask((ImageView) findViewById(R.id.img))
                 .execute(detailedContent[1]);
+
+        email.setText(detailedContent[2]);
+        website.setText(detailedContent[3]);
+
+        findViewById(R.id.layout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent j = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(j);
+
+            }
+        });
 
     }
 }
